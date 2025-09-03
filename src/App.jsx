d15bb830fe801595b1f768defedd893f4f749f35
rithish <rith172203k@gmail.com>
2025-09-03 14:25:27 +0530

    route

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,19 @@
 import Container from "./components/Container";
 import Card from "./components/Container";
-import {ToastContainer}  from "react-toastify"
+import { ToastContainer } from "react-toastify"
 import Loginform from "./components/Loginform";
+import { BrowserRouter, Routes, Route } from "react-router";
 const App = () => {
   return (
     <>
-    <Loginform/>
-      <Container />
-      <ToastContainer/>
-      
+      <BrowserRouter>
+        <Routes>
+          <Route path="/login" element={<Loginform />} />
+          <Route path="/" element={<Container />} />
+        </Routes>
+      </BrowserRouter>
+      <ToastContainer />
+
     </>)
 }
 export default App;
